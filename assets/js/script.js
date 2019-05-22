10eb4256475d477e54160499fb5c7d7a078b5337
Richard <benjamin.siskoo.gmail.com>
2019-05-22 15:46:18 +0200

    ajout des pages archives

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,7 @@ function fonctionLien()
 
 function toggle_div( btn, id_div )
 {
-  btn.innerHTML = (btn.innerHTML == '+')? '-' : '+';
+  btn.innerHTML = (btn.innerHTML == 'Masquer')? 'Lire l\'article' : 'Masquer';
   btn.title = (btn.title == 'Afficher la suite')? 'Masquer la suite' : 'Afficher la suite';
   document.getElementById( id_div ).style.display = (document.getElementById( id_div ).style.display == 'none')? 'block' : 'none';
 }
